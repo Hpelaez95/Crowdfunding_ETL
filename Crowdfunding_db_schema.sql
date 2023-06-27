c14c50a45d95151dee0482f48cb36714bcea0c88
Hpelaez95 <heriberto.pelaez@hotmail.com>
2023-06-26 23:56:10 -0500

    "final changes"

---
+++ Crowdfunding_db_schema.sql
@@ -0,0 +1,60 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/rZiwEu
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Categories" (
+    "category_id" varchar(30)   NOT NULL,
+    "category" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_Categories" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "Subcategories" (
+    "subcategory_id" varchar(30)   NOT NULL,
+    "subcategory" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_Subcategories" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "Campaigns" (
+    "cf_id" varchar(30)   NOT NULL,
+    "contact_id" varchar(30)   NOT NULL,
+    "company_name" varchar(50)   NOT NULL,
+    "description" varchar(90)   NOT NULL,
+    "goal" varchar(30)   NOT NULL,
+    "pledged" varchar(30)   NOT NULL,
+    "outcome" varchar(30)   NOT NULL,
+    "backers_count" varchar(30)   NOT NULL,
+    "country" varchar(30)   NOT NULL,
+    "currency" varchar(30)   NOT NULL,
+    "lauched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar(30)   NOT NULL,
+    "subcategory_id" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_Campaigns" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "Contacts" (
+    "contact_id" varchar(30)   NOT NULL,
+    "first_name" varchar(30)   NOT NULL,
+    "last_name" varchar(30)   NOT NULL,
+    "email" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "Campaigns" ADD CONSTRAINT "fk_Campaigns_category_id" FOREIGN KEY("category_id")
+REFERENCES "Categories" ("category_id");
+
+ALTER TABLE "Campaigns" ADD CONSTRAINT "fk_Campaigns_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Subcategories" ("subcategory_id");
+
+ALTER TABLE "Contacts" ADD CONSTRAINT "fk_Contacts_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Campaigns" ("contact_id");
+
