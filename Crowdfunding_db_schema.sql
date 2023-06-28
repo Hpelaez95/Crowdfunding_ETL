5169d385ea32c8de3d07b39d81e7824c843f4092
Hpelaez95 <heriberto.pelaez@hotmail.com>
2023-06-27 20:55:01 -0500

    "update tables"

--- Crowdfunding_db_schema.sql
+++ Crowdfunding_db_schema.sql
@@ -3,23 +3,23 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "Categories" (
+CREATE TABLE "category" (
     "category_id" varchar(30)   NOT NULL,
     "category" varchar(30)   NOT NULL,
-    CONSTRAINT "pk_Categories" PRIMARY KEY (
+    CONSTRAINT "pk_category" PRIMARY KEY (
         "category_id"
      )
 );
 
-CREATE TABLE "Subcategories" (
+CREATE TABLE "subcategory" (
     "subcategory_id" varchar(30)   NOT NULL,
     "subcategory" varchar(30)   NOT NULL,
-    CONSTRAINT "pk_Subcategories" PRIMARY KEY (
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
         "subcategory_id"
      )
 );
 
-CREATE TABLE "Campaigns" (
+CREATE TABLE "campaign" (
     "cf_id" varchar(30)   NOT NULL,
     "contact_id" varchar(30)   NOT NULL,
     "company_name" varchar(50)   NOT NULL,
@@ -34,27 +34,27 @@ CREATE TABLE "Campaigns" (
     "end_date" date   NOT NULL,
     "category_id" varchar(30)   NOT NULL,
     "subcategory_id" varchar(30)   NOT NULL,
-    CONSTRAINT "pk_Campaigns" PRIMARY KEY (
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
         "contact_id"
      )
 );
 
-CREATE TABLE "Contacts" (
+CREATE TABLE "contact" (
     "contact_id" varchar(30)   NOT NULL,
     "first_name" varchar(30)   NOT NULL,
     "last_name" varchar(30)   NOT NULL,
     "email" varchar(50)   NOT NULL,
-    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+    CONSTRAINT "pk_contact" PRIMARY KEY (
         "contact_id"
      )
 );
 
-ALTER TABLE "Campaigns" ADD CONSTRAINT "fk_Campaigns_category_id" FOREIGN KEY("category_id")
-REFERENCES "Categories" ("category_id");
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
 
-ALTER TABLE "Campaigns" ADD CONSTRAINT "fk_Campaigns_subcategory_id" FOREIGN KEY("subcategory_id")
-REFERENCES "Subcategories" ("subcategory_id");
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
 
-ALTER TABLE "Contacts" ADD CONSTRAINT "fk_Contacts_contact_id" FOREIGN KEY("contact_id")
-REFERENCES "Campaigns" ("contact_id");
+ALTER TABLE "contact" ADD CONSTRAINT "fk_contact_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "campaign" ("contact_id");
 
